f04e38d8aac1f498e9d7c61236b812d4588ab0fc
logtech26 <hengasam@zohomail.com>
2026-04-29 14:22:57 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponentComponent } from './about-component/about-component.component';
 import { HealthSystemService } from './health-system.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './features/login/login.component';
@@ -14,7 +13,6 @@ import { UserFormatPipe } from './shared/pipes/user-format.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponentComponent,
     LoginComponent,
     UserFormatPipe
   ],
